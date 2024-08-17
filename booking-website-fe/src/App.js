06d5178a36c6b8c6150d6c6a12ce10cbab2b9a8a
Khainq1911 <khaitom1911@gmail.com>
2024-08-17 18:11:29 +0700

    first commit

---
+++ booking-website-fe/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-yellow-200 text-3xl text-center bg-yellow-500">hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
